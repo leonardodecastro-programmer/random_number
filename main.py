3b6b19fe6ef29948b3133b666f41fa08dcb878f3
Leonardo de Castro <leonardo-programmer@proton.me>
2025-01-04 17:28:32 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+numero1 = random.randint(100, 999)
+numero2 = random.randint(100, 999)
+numero3 = random.randint(100, 999)
+
+resultado = f"{numero1}-{numero2}-{numero3}"
+
+print(resultado)
